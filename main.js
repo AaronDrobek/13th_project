768b8205595bc3a263266a8ed61fc11e29b9e4b4
Aaron Drobek <adrobek09@gmail.com>
2017-08-08 18:31:51 -0400

    bugs fixed

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-let one    = document.getElementById("one");
-let two    = document.getElementById("two");
-let three  = document.getElementById("three");
-let four   = document.getElementById("four");
-let five   = document.getElementById("five");
-let six    = document.getElementById("six");
-let seven  = document.getElementById("seven");
-let eight  = document.getElementById("eight");
-let search = document.getElementById("search");
 
-fetch("https://recipepuppyproxy.herokuapp.com/api/?q=bacon")
+let search    = document.getElementById("search");
+let button    = document.querySelector("button");
+  // console.log(search.value);
+
+// console.log(bar);
+
+
+button.addEventListener('click', function(){
+  let container = document.getElementById("container");
+
+  while(container.firstChild){
+    container.removeChild(container.firstChild);
+  }
+  let bar = search.value;
+fetch("https://recipepuppyproxy.herokuapp.com/api/?q=" + bar)
   .then(
     function(response) {
       if (response.status !== 200) {
@@ -18,25 +23,44 @@ fetch("https://recipepuppyproxy.herokuapp.com/api/?q=bacon")
       response.json().then(function(data) {
         // console.log("Here is the data:", data);
         for (var i = 0; i < data.results.length; i++) {
-          // console.log( "should be result", data.results[i]);
+          let el1 = document.createElement("div");
+          el1.classList.add("el1");
+          if (!data.results[i].thumbnail) {
+            el1.innerHTML = `<h1>${data.results[i].title}</h1>
+            <a href= "${data.results[i].href}"><img src="http://via.placeholder.com/350x150"></a>`;
+            container.appendChild(el1);
+          } else {
+            el1.innerHTML = `<h1 link="${data.results[i].href}">${data.results[i].title}</h1>
+            <a href= "${data.results[i].href}"><img src=${data.results[i].thumbnail}></a>`;
+            container.appendChild(el1);
+          }
+
+
+          //  create a div
+          //  add content
+          //  += header (url)
+          //  += img (url)
+          //  add myDiv to .container
         }
 
-          one.innerHTML += `<h1>${data.results[1].title}</h1>
-          <a href=${data.results[1].href}""><img src=${data.results[1].thumbnail}></a>`
-          two.innerHTML += `<h1>${data.results[2].title}</h1>
-          <a href=${data.results[2].href}""><img src=${data.results[2].thumbnail}></a>`
-          three.innerHTML += `<h1>${data.results[3].title}</h1>
-          <a href=${data.results[3].href}""><img src=${data.results[3].thumbnail}></a>`
-          four.innerHTML += `<h1>${data.results[5].title}</h1>
-          <a href=${data.results[5].href}""><img src=${data.results[5].thumbnail}></a>`
-          five.innerHTML += `<h1>${data.results[9].title}</h1>
-          <a href=${data.results[9].href}""><img src=${data.results[9].thumbnail}></a>`
-          six.innerHTML += `<h1>${data.results[6].title}</h1>
-          <a href=${data.results[6].href}""><img src=${data.results[6].thumbnail}></a>`
-          seven.innerHTML += `<h1>${data.results[7].title}</h1>
-          <a href=${data.results[7].href}""><img src=${data.results[7].thumbnail}></a>`
-          eight.innerHTML += `<h1>${data.results[8].title}</h1>
-          <a href=${data.results[8].href}""><img src=${data.results[8].thumbnail}></a>`
+
+
+          // one.innerHTML += `<h1>${data.results[1].title}</h1>
+          // <a href=${data.results[1].href}""><img src=${data.results[1].thumbnail}></a>`
+          // two.innerHTML += `<h1>${data.results[2].title}</h1>
+          // <a href=${data.results[2].href}""><img src=${data.results[2].thumbnail}></a>`
+          // three.innerHTML += `<h1>${data.results[3].title}</h1>
+          // <a href=${data.results[3].href}""><img src=${data.results[3].thumbnail}></a>`
+          // four.innerHTML += `<h1>${data.results[5].title}</h1>
+          // <a href=${data.results[5].href}""><img src=${data.results[5].thumbnail}></a>`
+          // five.innerHTML += `<h1>${data.results[9].title}</h1>
+          // <a href=${data.results[9].href}""><img src=${data.results[9].thumbnail}></a>`
+          // six.innerHTML += `<h1>${data.results[6].title}</h1>
+          // <a href=${data.results[6].href}""><img src=${data.results[6].thumbnail}></a>`
+          // seven.innerHTML += `<h1>${data.results[7].title}</h1>
+          // <a href=${data.results[7].href}""><img src=${data.results[7].thumbnail}></a>`
+          // eight.innerHTML += `<h1>${data.results[8].title}</h1>
+          // <a href=${data.results[8].href}""><img src=${data.results[8].thumbnail}></a>`
 
       });
     }
@@ -44,3 +68,4 @@ fetch("https://recipepuppyproxy.herokuapp.com/api/?q=bacon")
   .catch(function(err) {
     console.log("Fetch Error :-S", err);
   });
+})
